9a377fc060d97071c95e29e0a4e13b173b3638f1
Cheng <lichchchn@gmail.com>
2021-03-21 22:10:17 +0800

    add tcp_echo example

---
+++ example/tcp_echo.cpp
@@ -0,0 +1,73 @@
+//
+// Created by Cheng on 2021/3/21.
+//
+
+#include "simio.h"
+
+#include <iostream>
+#include <cerrno>
+
+#include <cstring>
+#include <thread>
+#include <pthread.h>
+#include <unistd.h>
+#include <map>
+
+using namespace simio;
+using namespace std;
+
+Task on_accept(const std::pair<simio::TcpStream, simio::SocketAddr> &pair) {
+    cout << "Tcp accept: " << pair.second << endl;
+    return Task(pair.first);
+}
+
+void echo(Task *task) {
+    TcpStream stream = task->inner_;
+    string buf(1024, '\0');
+    int num = stream.recv(buf);
+    if (num == 0) {
+        cout << "server close" << endl;
+        task->close();
+        return;
+    }
+    cout << "server recv: " << buf << endl;
+    stream.send(buf);
+    cout << "server send: " << buf << endl;
+}
+
+void client(int index) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    TcpStream stream = TcpStream::connect(SocketAddr("127.0.0.1:5000"));
+    stream.set_nonblocking(false);
+    for (int i = 0; i < 10; i++) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        string buf("Client");
+        buf.push_back(char(index + '0'));
+        buf.append(" send the number: ");
+        buf.push_back(char(i + '0'));
+        stream.send(buf);
+        cout << "client send: " << buf << endl;
+        string rbuf(1024, '\0');
+        int num = stream.recv(rbuf);
+        if (num == 0) {
+            cout << "client close" << endl;
+            return;
+        }
+        cout << "client recv: " << buf << endl;
+    }
+    cout << "client close" << endl;
+}
+
+int main() {
+    std::vector<std::thread> vec{};
+    for (int i = 1; i < 2; i++) {
+        vec.emplace_back(client, i);
+    }
+
+    SocketAddr addr("127.0.0.1:5000");
+    TcpServer server(addr);
+    server.set_accept_callback(on_accept);
+    server.set_stream_callback(echo);
+    server.start();
+
+}
